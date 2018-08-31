87bc3fefeefc71221776d25bd780607d7a627ba0
Maid Dog <maiddogsrl@gmail.com>
2018-08-31 16:07:59 -0500

    Add benchmarks and criterion

---
+++ benches/extract.rs
@@ -0,0 +1,27 @@
+#[macro_use] extern crate criterion;
+extern crate nds;
+
+use criterion::Criterion;
+
+fn bench_extract_big(c: &mut Criterion) {
+    use nds::Extractor;
+
+    let ex = Extractor::new("big.nds").unwrap();
+
+    c.bench_function("extract big", move |b| b.iter(|| {
+        ex.extract("tmp/big").unwrap();
+    }));
+}
+
+fn bench_extract_small(c: &mut Criterion) {
+    use nds::Extractor;
+
+    let ex = Extractor::new("small.nds").unwrap();
+
+    c.bench_function("extract small", move |b| b.iter(|| {
+        ex.extract("tmp/small").unwrap();
+    }));
+}
+
+criterion_group!(benches, bench_extract_big, bench_extract_small);
+criterion_main!(benches);
