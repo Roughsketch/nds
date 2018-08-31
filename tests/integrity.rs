f61aacc562c23170d3967c4ef81aa8d3ee022caf
Maid Dog <maiddogsrl@gmail.com>
2018-08-30 23:12:20 -0500

    Make integrity test clean up after itself

--- tests/integrity.rs
+++ tests/integrity.rs
@@ -5,10 +5,29 @@ extern crate nds;
 mod tests {
     use super::nds::{Extractor, Builder};
     use super::md5::compute;
+    use std::panic;
 
     #[test]
     fn built_rom_is_same() {
-        use std::fs::{read, remove_dir_all, remove_file};
+        run_test(_built_rom_is_same, _built_rom_is_same_cleanup);
+    }
+
+    fn run_test<T, U>(test: T, cleanup: U) -> ()
+        where
+            T: FnOnce() -> () + panic::UnwindSafe,
+            U: FnOnce() 
+    {
+        let result = panic::catch_unwind(|| {
+            test()
+        });
+
+        cleanup();
+
+        assert!(result.is_ok());
+    }
+
+    fn _built_rom_is_same() {
+        use std::fs::read;
 
         let extractor = Extractor::new("small.nds")
             .expect("Could not make Extractor");
@@ -31,9 +50,13 @@ mod tests {
         let original_md5 = compute(&original);
         let built_md5 = compute(&built);
 
-        remove_dir_all("output").unwrap();
-        remove_file("built.nds").unwrap();
-
         assert!(&original_md5 == &built_md5);
+    }
+
+    fn _built_rom_is_same_cleanup() {
+        use std::fs::{remove_dir_all, remove_file};
+
+        let _ = remove_dir_all("output");
+        let _ = remove_file("built.nds");
     }
 }
