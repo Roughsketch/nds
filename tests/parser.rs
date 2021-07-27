660b04c50f71e3b28ffa88e4ba00bd2af9c5e3e5
TornaxO7 <tornax07@gmail.com>
2021-07-27 10:17:07 +0200

    Removed Mario Kart NDS

--- tests/parser.rs
+++ tests/parser.rs
@@ -1,7 +1,11 @@
 use nds::parser::NDSParser;
 use std::convert::TryFrom;
 
+// our testing .nds files
+const TEST_HELLO_WORLD: &'static str = "tests/test_nds_files/hello_world.nds";
+const TEST_3D_BOTH_SCREENS: &'static str = "tests/test_nds_files/3D_Both_Screens.nds";
+
 #[test]
 fn test_parsing() {
-    assert!(NDSParser::try_from("test_nds_files/mario_kart_ds.nds").is_ok());
+    assert!(NDSParser::try_from(TEST_HELLO_WORLD).is_ok());
 }
